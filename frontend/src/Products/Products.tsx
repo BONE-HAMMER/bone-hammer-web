170765e20820f48cd6c5449ce3b6b6126ed00d7f
Byron Justice <157338394+byronjustice1@users.noreply.github.com>
2024-04-18 17:01:43 -0400

    Update Products.tsx

--- frontend/src/Products/Products.tsx
+++ frontend/src/Products/Products.tsx
@@ -1,10 +1,23 @@
 
-import data from "./data";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+type Product = {
+    id: number;
+    name: string;
+    description: string;
+    brand: string;
+    price: number;
+    rating: number;
+    numberOfReviews: number;
+    imageUrl: string;
+}
+
 function Products() {
     return (
     <div className="content">
     <ul className="products">
-{data.products.map((product) => (
+{products.map((product) => (
 <li>
 <div className="product">
 <img
@@ -28,5 +41,16 @@ alt="product"
     </div>
     
     );
+    const [products, setProducts]: [Product[], (products: Product[]) => void] = useState(emptyProducts);
+    useEffect(() => {
+        axios.get<Product[]>("http://localhost:7520/catalog", 
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((response) => setProducts(response.data))
+        .catch((error) => console.log(error));
+    }, []);
 }
     export default Products;
